b1e296bb263b815fdcb3e3aacc330cbecebdba33
Zoe <zoemk07@gmail.com>
2023-04-16 22:49:54 -0700

    C

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+let smallEl = document.getElementById("small");
+let mediumEl = document.getElementById("medium");
+let largeEl = document.getElementById("large");
 
-    document.getElementById("button1").addEventListener("click", function () {
+let displaypriceEl = document.getElementById("displayprice");
+let toppingsEl = document.getElementById("toppings");
 
-        document.getElementById("textbox1").value = "success";
-    });
-});
+function submit() {
+    let price = 0;
+    if (smallEl.checked) {
+        price += 7;
+    }
+
+    if (mediumEl.checked) {
+        price += 10;
+    }
+
+    if (largeEl.checked) {
+        price += 14;
+    }
+
+    if (toppingsEl.selectedIndex == 1) {
+        price += 2;
+    }
+
+    if (toppingsEl.selectedIndex == 2) {
+        price += 3;
+    }
+
+    
+    displaypriceEl.innerHTML = "$"+ price;
+}
 
